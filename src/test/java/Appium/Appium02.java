e8fa39c7b17ed338367150f94d76e1f94415341a
firdevs <ffirdevskaya31@gmail.com>
2022-03-06 15:00:13 +0300

    first commit good

--- src/test/java/Appium/Appium02.java
+++ src/test/java/Appium/Appium02.java
@@ -22,18 +22,25 @@ public class Appium02 {
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"Android");
         desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Emulator");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11.0");
+
         //eger appActivity ve appPackage kullanacaksaniz app path gerekli degil
-        //  desiredCapabilities.setCapability(MobileCapabilityType.APP,"C:\Users\erdis\IdeaProjects\appium1\apps\GestureTool.apk");
+          desiredCapabilities.setCapability(MobileCapabilityType.APP,"C:/Users/erdis/IdeaProjects/appium1/apps/apiDemos.apk");
         //app acilirken izin istediginde appActivity ve appPackage tanimlayarak bu sorunu gecebilirsiniz.
         // app telefonda acin
         //terminal veya CMD prompt acarak adb shell  yazin
         //sonra dumpsys window | grep -E "mCurrentFocus" komutunu girice appActivity ve appPackage bilgilerini ulasabilirsiniz
-        desiredCapabilities.setCapability("appPackage","com.touchboarder.android.api.demos");
-        desiredCapabilities.setCapability("appActivity","com.touchboarder.androidapidemos.MainActivity");
+        // desiredCapabilities.setCapability("appPackage","com.touchboarder.android.api.demos");
+        //desiredCapabilities.setCapability("appActivity","com.touchboarder.androidapidemos.MainActivity");
 //http://localhost:4723/wd/hub
         AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL ("http://127.0.0.1:4723/wd/hub"),desiredCapabilities);
 
+        if(driver.isDeviceLocked()) {
+           driver.unlockDevice();
+        }
+
         Thread.sleep(4000);
+
+
 
 }
     }
